891e60054a283e5b96db3535360125a734bbb57c
Will Andre <wandre94@gmail.com>
2020-12-22 23:46:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  katz_deli.each_with_index do |name, i| 
+  katz_deli.each_with_index do |name, i|
   if katz_deli == []
     puts "The line is currently empty."
   elsif i > 0
