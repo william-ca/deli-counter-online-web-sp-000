d6ad97dd7b99e17e9a82e54515fb1eaf097f5799
Will Andre <wandre94@gmail.com>
2020-12-22 23:52:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,13 @@ katz_deli = []
 
 def line(katz_deli)
   katz_deli.each_with_index do |name, i|
-  if i < 0
+  if katz_deli.length < 0
     puts "The line is currently empty."
   elsif i >= 0
     puts "The line is currntly: #{i+1}. #{name}"
   end
   end
 end
+
+def take_a_number(katz_deli)
+  
