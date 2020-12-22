18ac7cf7bdbf9c5aa0a93d78de7bd5baa48f874e
Will Andre <wandre94@gmail.com>
2020-12-22 23:53:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   katz_deli.each_with_index do |name, i|
   if katz_deli.length < 0
-    puts "The line is currently empty."
+    return "The line is currently empty."
   elsif i >= 0
     puts "The line is currntly: #{i+1}. #{name}"
   end
